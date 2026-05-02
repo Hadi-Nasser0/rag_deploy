280617770bed99e75d25c7d7f5e2bc01165d1578
Hadi <hadinasser2.01@gmail.com>
2026-05-02 13:48:22 +0200

    testing the app using sqlite

--- src/database/session.py
+++ src/database/session.py
@@ -5,16 +5,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
+# SQLite configuration for local development (Step 4)
+DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///./test.db')
 # Replace credentials with yours
-DATABASE_URL = os.getenv('DATABASE_URL',"postgresql+psycopg2://rag_role:mypassword@localhost/rag_db")
+#DATABASE_URL = os.getenv('DATABASE_URL',"postgresql+psycopg2://rag_role:mypassword@localhost/rag_db")
 
-# engine creation
-engine = create_engine(DATABASE_URL)
+if 'sqlite' in DATABASE_URL:
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread" : False})
+else: 
+    # PostgreSQL creation
+    engine = create_engine(DATABASE_URL)
 
 # session class config
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 #Base class for ORM models
 Base = declarative_base()
-
